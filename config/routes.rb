cd75e254cf8a20d0b8ccdc4972162cbd7e547887
Daniel Pearson <pearsondg@gmail.com>
2022-11-16 20:35:32 +0000

    yelp MVC working, with restaurant and reviews usable

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,14 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'restaurants#index'
+
+  get 'restaurants', to: 'restaurants#index'
+  get 'restaurants/new', to: 'restaurants#new'
+  get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+  post 'restaurant', to: 'restaurants#create'
+
+  resources :restaurants do
+    resources :reviews, only: %i[new create]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", phone_number: "07895 784632", category: "chinese" }
+pizza_east = { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", phone_number: "07895 784632", category: "italian" }
+nandos = { name: "Nandos", address: "42 Whisper Ln, London E3 4DG,", phone_number: "07895 784632", category: "belgian" }
+rasa = { name: "Rasa", address: "10 Church Street, London, E8 2PJ", phone_number: "07895 784632", category: "french" }
+good_egg = { name: "Good Egg", address: "17 Church Street, London, E8 2PK", phone_number: "07895 784632", category: "japanese" }
+
+[dishoom, pizza_east, nandos, rasa, good_egg].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
+
+puts "making reviews..."
+dishoom = { rating: 5, content: "It is super nice and I like it" }
